42901eb45615a8f07f5b9e7699a6feb5781fd726
Diego <diegodecstraid@gmail.com>
2025-05-09 00:19:21 -0300

    Exercicio 1 Feito

--- exercicios-modulo-01/exercicio-1.js
+++ exercicios-modulo-01/exercicio-1.js
@@ -1,1 +1,14 @@
 //Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
+
+const prompt = require('prompt-sync')();
+
+//Vamos criar uma variável para armazenar o número inteiro
+let numero = Number(prompt('Digite um número inteiro: '));
+
+//Vamos verificar se o número é par ou ímpar
+if (numero % 2 === 0) {
+    console.log(`O número ${numero} é par.`);
+} else {
+    console.log(`O número ${numero} é ímpar.`);
+}
+//Vamos testar o código
