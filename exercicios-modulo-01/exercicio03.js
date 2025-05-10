50cda39189bc206117b7a57019210024bca4a6cb
Diego <diegodecstraid@gmail.com>
2025-05-10 14:36:49 -0300

    Realização do Exercicio 03

--- exercicios-modulo-01/exercicio03.js
+++ exercicios-modulo-01/exercicio03.js
@@ -1,1 +1,15 @@
 //Implemente um programa que recebe uma nota de 0 a 10 e classifica como "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+
+const prompt = require('prompt-sync')();
+
+//Crie uma variável para armazenar a nota
+let nota = Number(prompt('Digite a sua nota (0 a 10): '));
+
+//Verificação da nota
+if (nota >= 0 && nota < 4) {
+    console.log('Você foi reprovado');
+} else if (nota >= 4 && nota < 7) {
+    console.log('Você está de recuperação');
+} else {
+    console.log('Você foi aprovado');
+}
