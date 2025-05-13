6d7815a9172d1d1298f22c7bccb664e7382d371b
Diego <diegodecstraid@gmail.com>
2025-05-13 00:15:56 -0300

    Feito os Exercicios 04 e 05

--- exercicios-modulo-01/exercicio04.js
+++ exercicios-modulo-01/exercicio04.js
@@ -1,1 +1,37 @@
 //Crie um menu interativo no console que oferece ao usuário a escolha de três opções. Utilize switch-case para implementar a lógica de cada opção selecionada
+
+
+const prompt = require('prompt-sync')();
+
+// Menu de opções
+console.log('Escolha uma opção:');
+console.log('1. Adição');
+console.log('2. Subtração');
+console.log('3. Multiplicação');
+
+// Lê a opção escolhida pelo usuário
+let opcao = prompt('Digite o número da opção desejada: ');
+
+// Executa a ação correspondente à opção escolhida
+switch (opcao) {
+    case '1':
+        console.log('Você escolheu Adição');
+        let num1 = parseFloat(prompt('Digite o primeiro número: '));
+        let num2 = parseFloat(prompt('Digite o segundo número: '));
+        console.log(`Resultado: ${num1 + num2}`);
+        break;
+    case '2':
+        console.log('Você escolheu Subtração');
+        let num3 = parseFloat(prompt('Digite o primeiro número: '));
+        let num4 = parseFloat(prompt('Digite o segundo número: '));
+        console.log(`Resultado: ${num3 - num4}`);
+        break;
+    case '3':
+        console.log('Você escolheu Multiplicação');
+        let num5 = parseFloat(prompt('Digite o primeiro número: '));
+        let num6 = parseFloat(prompt('Digite o segundo número: '));
+        console.log(`Resultado: ${num5 * num6}`);
+        break;
+    default:
+        console.log('Opção inválida');
+}
--- exercicios-modulo-01/exercicio05.js
+++ exercicios-modulo-01/exercicio05.js
@@ -1,1 +1,31 @@
 //Escreva um programa que calcula o Índice de Massa Corporal (IMC) de uma pessoa e determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade) utilizando if-else.
+
+const prompt = require('prompt-sync')();
+
+// Função para calcular o IMC
+function calcularImc(peso, altura) {
+    return (peso / (altura * altura));
+}
+
+// Recebe o peso e a altura do usuário
+let altura = Number(prompt('Digite sua altura (em metros): '));
+let peso = Number(prompt('Digite seu peso (em kg): ')); 
+
+// Calcula o IMC
+let imc = calcularImc(peso, altura);
+
+// Determina a categoria de peso
+let categoria;
+if (imc < 21) {
+    categoria = 'Baixo peso';
+} else if (imc >= 21 && imc < 30) {
+    categoria = 'Peso normal';
+} else if (imc >= 30 && imc < 60) {
+    categoria = 'Sobrepeso';
+} else {
+    categoria = 'Obesidade';
+}
+
+// Exibe o resultado
+console.log(`Seu IMC é ${imc.toFixed(2)} e você está na categoria: ${categoria}`);
+
